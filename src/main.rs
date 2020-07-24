581654a72449574d70e2db7cdd945edda7fa1be5
Andy Alt <andy5995@users.noreply.github.com>
2020-07-24 18:37:12 -0500

    feat: add -V, --version option
    * add support for command line options
    
    * add --version option
    
    * Cargo.toml:add co-author @Jammyjamjamman
    
    * Cargo.toml: add project description

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
 mod physics;
 use physics::{converter, Item};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// Activate debug mode
+    // short and long flags (-d, --debug) will be deduced from the field's name
+    #[structopt(short, long)]
+    debug: bool,
+
+    // Show version
+    #[structopt(short = "V", long = "version")]
+    version: bool,
+}
+
+fn display_package_info() {
+    println!(
+        "{} version: {}",
+        env!("CARGO_PKG_NAME"),
+        env!("CARGO_PKG_VERSION")
+    );
+    println!();
+}
 
 fn main() {
     // should the weight be displayed using the metric system?
@@ -22,6 +44,11 @@ fn main() {
             mass: 0.05,
         },
     ];
+
+    let opt = Opt::from_args();
+    if opt.version {
+        display_package_info();
+    }
 
     let energy = data[0].to_energy();
     println!("mass: {}kg, energy: {}J", data[0].mass, energy.0);
